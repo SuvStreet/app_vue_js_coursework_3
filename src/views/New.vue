cebad8cc53b30fbedea1928d3404db6251e321d4
Dmitrij Kolotusha <suvstreet@gmail.com>
2021-12-27 22:30:35 +0100

    valid button create new task

--- src/views/New.vue
+++ src/views/New.vue
@@ -3,23 +3,42 @@
     <h1>Создать новую задачу</h1>
     <div class="form-control">
       <label for="title">Название</label>
-      <input type="text" id="title">
+      <input type="text" id="title" v-model.trim="validTitle" />
     </div>
 
     <div class="form-control">
       <label for="date">Дата дэдлайна</label>
-      <input type="date" id="date">
+      <input type="date" id="date" v-model="validDate" />
     </div>
 
     <div class="form-control">
       <label for="description">Описание</label>
-      <textarea id="description"></textarea>
+      <textarea id="description" v-model.trim="validDescription"></textarea>
     </div>
 
-    <button class="btn primary">Создать</button>
+    <button class="btn primary" :disabled="isDisabled">Создать</button>
   </form>
 </template>
 
-
 <script>
+import { computed, reactive, toRefs } from '@vue/reactivity'
+
+export default {
+  setup() {
+    const dataInput = reactive({
+      validTitle: '',
+      validDate: '',
+      validDescription: '',
+    })
+
+    const isDisabled = computed(() => {
+      return !Object.values(dataInput).every((value) => value.length > 0)
+    })
+
+    return {
+      isDisabled,
+      ...toRefs(dataInput),
+    }
+  },
+}
 </script>
